24e02410f427f44de6e208bba52df5c76b277a4c
Fernandez Chris <chris.fernandez@bugcrowd.com>
2017-05-24 16:43:00 -0700

    updated i2p install

--- install-i2p.sh
+++ install-i2p.sh
@@ -1,4 +1,5 @@
-sudo apt-add-repository ppa:i2p-maintainers/i2p
+sudo add-apt-repository ppa:purplei2p/i2pd
 sudo apt-get update
-sudo apt-get install i2p
-sudo dpkg-reconfigure i2p
+sudo apt-get upgrade
+sudo apt-get install i2pd
+sudo systemctl status i2pd
