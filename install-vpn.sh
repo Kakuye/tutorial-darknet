0a5452df54ba162ae643f3061898748ae18b347b
Fernandez Chris <chris.fernandez@bugcrowd.com>
2017-05-24 17:45:03 -0700

    more updates

--- install-vpn.sh
+++ install-vpn.sh
@@ -6,6 +6,7 @@ gunzip -c /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz > /
 cp server.conf /etc/openvpn/server.conf
 echo 1 > /proc/sys/net/ipv4/ip_forward
 cp sysctl.conf /etc/sysctl.conf
+sysctl -p
 cp ufw /etc/default/ufw
 cp before.rules /etc/ufw/before.rules
 cp -r /usr/share/easy-rsa/ /etc/openvpn/
@@ -26,12 +27,14 @@ source /etc/openvpn/easy-rsa/vars
 /etc/openvpn/easy-rsa/build-key-server server
 
 
-cp /etc/openvpn/easy-rsa/keys/{server.crt,server.key,ca.crt} /etc/openvpn
+cp /etc/openvpn/easy-rsa/keys/{server.crt,server.key,ca.crt,ca.key} /etc/openvpn
 
-ls /etc/openvpn
 
-service openvpn start
+echo "***THIS MAY TAKE A WHILE***"
+/etc/openvpn/build-dh
+openvpn --genkey --secret keys/ta.key
 
-service openvpn status
+cp /etc/openvpn/easy-rsa/keys/{ta.key,dh2048.pem} /etc/openvpn
 
-service openvpn enable
+systemctl start openvpn@server
+systemctl status openvpn@server
