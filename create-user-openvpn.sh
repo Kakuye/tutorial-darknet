c6c0d9a03f1ef52e09d3562bf641ab5c1989bef7
Fernandez Chris <chris.fernandez@bugcrowd.com>
2017-05-24 18:42:44 -0700

    fix path

--- create-user-openvpn.sh
+++ create-user-openvpn.sh
@@ -14,7 +14,7 @@ cd /etc/openvpn/easy-rsa/ #this is just in case
 [[ -d /etc/openvpn/easy-rsa/users ]] || mkdir /etc/openvpn/easy-rsa/users
 
 mkdir /etc/openvpn/easy-rsa/users/$1
-cat /etc/openvpn/easy-rsa/keys/client.ovpn > /etc/openvpn/easy-rsa/users/$1/$1.ovpn
+cat /etc/openvpn/client.ovpn > /etc/openvpn/easy-rsa/users/$1/$1.ovpn
 echo "<ca>" >> /etc/openvpn/easy-rsa/users/$1/$1.ovpn
 cat /etc/openvpn/easy-rsa/keys/ca.crt >> /etc/openvpn/easy-rsa/users/$1/$1.ovpn
 echo "</ca>" >> /etc/openvpn/easy-rsa/users/$1/$1.ovpn
