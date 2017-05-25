4a68e6d6e01baaeab5ed3f8117469a1f2f28b5c1
Fernandez Chris <chris.fernandez@bugcrowd.com>
2017-05-24 19:33:35 -0700

    weechat2

---
+++ install-weechat.sh
@@ -0,0 +1,6 @@
+apt-get install -y weechat
+mkdir ~/.weechat
+cp ~/tutorial-darnet/irc.conf ~/.weechat/irc.conf
+echo "That's it. you can run now weechat and it will auto connect to 10.8.0.1 port 6668"
+echo "it will auto connect to some channels like #hispagatos"
+echo "it may take a while since your i2p server is only running 1-2 hours or less.."
